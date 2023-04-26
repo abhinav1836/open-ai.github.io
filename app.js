6c16fe1a4e681187e80a23d1bb20bdd41add0483
abhinav1836 <129741919+abhinav1836@users.noreply.github.com>
2023-04-26 16:32:40 +0530

    first

---
+++ app.js
@@ -0,0 +1,52 @@
+import API_KEY from "./apikeys";
+
+const submitButton= document.querySelector('#submit');
+const outputElement= document.querySelector('#output');
+const inputElement= document.querySelector('input');
+const historyElement= document.querySelector('.history');
+const buttonElement= document.querySelector('button');
+
+function changeInput(value){
+    const inputElement = document.querySelector('input');
+    inputElement.value=value;
+
+}
+
+async function getMessage(){
+    const options = {
+        method:'POST',
+        headers: {
+            'Authorization': `Bearer ${API_KEY}`,
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify({
+                model: "gpt-3.5-turbo",
+                messages: [{role: "user", content: inputElement.value}],
+                max_tokens:100
+              })      
+    };
+
+    try{
+        const response = await fetch('https://api.openai.com/v1/chat/completions',options)
+        const data =await response.json()
+        console.log(data)
+        outputElement.textContent = data.choices[0].message.content;
+        if (data.choices[0].message.content && inputElement.value){
+            const pele=document.createElement('p')
+            pele.textContent=inputElement.value;
+            pele.addEventListener('click', ()=> changeInput(pele.textContent))
+            historyElement.append(pele)
+        }
+    }catch (error){
+        console.error(error);
+
+    }
+}
+
+submitButton.addEventListener('click', getMessage)
+
+function clearInput(){
+    inputElement.value='';
+
+}
+buttonElement.addEventListener('click', clearInput)
